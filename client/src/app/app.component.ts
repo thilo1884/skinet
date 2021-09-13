a611aa85c1136d40c01f19412e938443ab00fc2f
Paola Cantamesse <paola.cantamesse@gmail.com>
2021-09-13 22:41:10 +0200

    End section8. Typescript

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; //manually added
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit{
+  title = 'Skinet'; //class property
+  products: IProduct[]; //array that take any kind of type. It was any[]
+
+  // In the constructore is where we inject the http client into here
+  constructor(private http: HttpClient){}
+
+   
+  ngOnInit(): void {
+    //here is where we call our API
+
+    //console.log(response);
+
+    //this returns an observable
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+  
 }
